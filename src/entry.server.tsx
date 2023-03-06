cc3b48e5558809864b76deb971878458ad681471
Zakuru San <zelmarou@gmaiL.com>
2023-03-07 01:38:25 +0700

    Update entry.server.tsx

--- src/entry.server.tsx
+++ src/entry.server.tsx
@@ -3,7 +3,18 @@ import { HandleDocumentRequestFunction, RemixServer } from 'remix';
 import type { EntryContext } from 'remix';
 import dotenv from 'dotenv';
 
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM('null', {
+  pretendToBeVisual: false,  url: "http://192.168.0.122/" 
+});
+
 dotenv.config();
+dom.window.setTimeout = global.setTimeout;
+Object.keys(dom.window).forEach((e: string) => {
+  if((global as any)[e as any] === undefined){
+    (global as any)[e] = dom.window[e]
+  }
+});
 
 export const handleRequest: HandleDocumentRequestFunction = (
   request: Request,
